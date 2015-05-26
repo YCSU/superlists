6d89f5f183111e78c5a0a1a6c0ff39849c7a2076
vs <victor.su@azurewave.com>
2015-05-26 16:26:44 +0800

    new URL, view and display lists

--- superlists/urls.py
+++ superlists/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'lists.views.home_page', name='home'),
+    url(r'^lists/the_only_list/$', 'lists.views.view_lists'),
     # url(r'^blog/', include('blog.urls')),
 
     #url(r'^admin/', include(admin.site.urls)),
